6e3ac803a1307137ebe3cbefa48003d1c3ee9095
Raphael Forment <raphael.forment@gmail.com>
2023-07-18 00:17:58 +0200

    test again

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ const { execSync } = require("child_process");
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('./src/img');
+  eleventyConfig.addPassthroughCopy('./src/css/');
   eleventyConfig.setServerOptions({
     watch: ['./public/css/styles.css']
   });
